2f8e8d603497e81064faa014d3c16429febcc291
Jowwy02 <ukpejosephineabel@gmail.com>
2022-11-27 14:02:49 -0800

    Add javascript file and code for hamburger button.

---
+++ index.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+const navLinksWrap = document.querySelector(".nav-links");
+
+//add EventListener
+//when clicked display nav items
+//when clicked create and display close icon 
+//when clicked hide hamBtn
+//create a function that creates the close icon
+//create image tag and set the src and alt
+//create div tag and set the class name 
+//append the image tag to the div tag
+//return the div tag
+
+hamburger.addEventListener('click', () => {
+    navLinksWrap.style.display = "block"
+})
