6e425de54867610574a09ce00498700896734442
Jowwy02 <ukpejosephineabel@gmail.com>
2023-04-26 02:27:16 -0700

    Add popup modal and create modal cards.

--- index.js
+++ index.js
@@ -12,13 +12,12 @@
 //append the image tag to the div tag
 //return the div tag
 
-
 const hamburger = document.querySelector(".hamburger");
 const navLinksWrap = document.querySelector(".nav-links");
 
 function createCloseIcon() {
   const newDiv = document.createElement("div");
-  newDiv.className = "closeIcon"
+  newDiv.className = "closeIcon";
   // const image = document.createElement('img');
   const closeIcon = `<svg width="14" height="15" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M2.404.782l11.314 11.314-2.122 2.122L.282 2.904z"/><path d="M.282 12.096L11.596.782l2.122 2.122L2.404 14.218z"/></g></svg>`;
   newDiv.innerHTML = closeIcon;
@@ -35,7 +34,33 @@ hamburger.addEventListener("click", () => {
   closeIcon.addEventListener("click", () => {
     navLinksWrap.style.display = "none";
     navWrap.classList.remove("nav-wrapper-bg");
-    hamburger.style.display = "block"
-    closeIcon.remove()
-  })
+    hamburger.style.display = "block";
+    closeIcon.remove();
+  });
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
